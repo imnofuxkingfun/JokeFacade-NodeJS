4920ddf4380f8ed7c12c57adfb8213f8f9e8a8b8
MIHAI DANAILA <danailateodor@yahoo.com>
2026-01-13 20:24:05 +0200

    basic header

--- play-this/app/layout.tsx
+++ play-this/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/authContext";
 import { getSessionUser } from "@/actions/auth";
+import Header from "@/components/shared/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,9 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        
         <AuthProvider initialUser={sessionUser}>
+          <Header />
         {children}
         </AuthProvider>
       </body>
---
+++ play-this/components/shared/header.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { useAuth } from '@/context/authContext';
+const Header = () => {
+    const { user } = useAuth();
+
+    return(
+        <header className="w-100">
+            <nav className="flex gap-4 p-4 border-b">
+                <a href="/">Home</a>
+                {user ?(
+                    <a href="/profile">Profile</a>
+                ) : (
+                    <>
+                    <a href="/login">Login</a>
+                    <a href="/register">Register</a>
+                    </>
+                )}
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
