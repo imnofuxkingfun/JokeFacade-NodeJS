3b4e72d7e5d7c56cfd7cc977b72d15931e6a8cd8
Luca <ralu.exe@gmail.com>
2026-01-14 04:57:41 +0200

    css artist and profile update

--- play-this/app/profile/page.tsx
+++ play-this/app/profile/page.tsx
@@ -1,20 +1,36 @@
+// Update app/profile/page.tsx
+
 'use client'
 
 import { useAuth } from "@/context/authContext";
 import LikedSongsList from "@/components/songs/likedSongsList";
+import styles from './profile.module.css';
 
 export default function ProfilePage() {
     const { logout, user } = useAuth();
 
     return(
-        <div>
-            <h1>Profile</h1>
-            <h2>username: {user?.username}</h2>
-            <h2>email: {user?.email}</h2>
+        <div className={styles.container}>
+            <div className={styles.profileHeader}>
+                <h1>My Profile</h1>
+                
+                <div className={styles.userInfo}>
+                    <div className={styles.infoCard}>
+                        <label>Username</label>
+                        <p>{user?.username}</p>
+                    </div>
+                    <div className={styles.infoCard}>
+                        <label>Email</label>
+                        <p>{user?.email}</p>
+                    </div>
+                </div>
+
+                <button onClick={logout} className={styles.logoutBtn}>
+                    Logout
+                </button>
+            </div>
 
             <LikedSongsList />
-
-            <button onClick={logout}>Logout</button>
         </div>
     );
 }
